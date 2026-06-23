e27181cd3d41c894036a74b617c0c566164d9b23
95ru <afb95@rutgers.edu>
2026-06-23 13:48:47 -0400

    chore: added test for python to socket

--- robot.py
+++ robot.py
@@ -3,9 +3,11 @@ from NLG import NLG
 from deliberate import StoryEngine
 from graph import StoryGraph
 from context import DiscourseContext
+import socket
+import struct
 
 SCRIPT = "wound_cleaning.json"
-
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 class Robot:
     def __init__(self, script=SCRIPT):
@@ -40,7 +42,7 @@ class Robot:
             elif out.control is not None:
                 print("Robot: [continues in silence]")
             if out.control is not None:
-                print(out.control)
+                sock.sendto(struct.pack("<dd", 1, 2), ("127.0.0.1", 5005))
 
 
 QUIT_COMMANDS = ["quit", "exit"]
@@ -63,4 +65,5 @@ def read_user_input():
 
 if __name__ == "__main__":
     print("Press Enter to let the robot continue, or type a response. Type 'quit' or press Ctrl+C to stop.")
+    sock.sendto(struct.pack("<dd", 1, 2), ("127.0.0.1", 5005))
     Robot().run()
